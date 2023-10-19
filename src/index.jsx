012beafa96558a1488a0936b8a6f8a398669808a
cashartle1 <139818745+cashartle1@users.noreply.github.com>
2023-10-19 13:43:00 -0500

    imports MainView component
    imports MainView component and removes comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
 
 import "./index.scss";
 
-//Main component (will eventually use all the others)
 const MyFlixApplication = () => {
     return (
-        <div className="my-flix">
-            <div>Good morning</div>
-        </div>
+        <MainView />
     );
 };
 
