ec6ad16215d3689823ff8cc22bc4c6ba5540ba30
cashartle1 <139818745+cashartle1@users.noreply.github.com>
2023-11-21 20:33:27 -0600

    Adds favorite movie buttons
    adds "add favorite movie" button and "remove favorite movie" buttons to movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,8 +4,69 @@ import PropTypes from 'prop-types';
 import { Button, Card, Image } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+
 export const MovieView = ({ movies, user, token, setUser }) => {
     const { movieId } = useParams();
+    const movie = movies.find((item) => item.id === movieId);
+
+    const [isFavorite, setIsFavorite] = useState(
+        user.FavoriteMovies.includes(movie.id)
+    );
+
+    const addFavoriteMovie = () => {
+
+        fetch(
+            `https://movie-flix-f31fbb6efa26.herokuapp.com/users/${user.Username}/movies/${movie.id}`,
+            { method: "POST", headers: { Authorization: `Bearer ${token}` } }
+        )
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    alert("Failed to add favorite movie");
+                    console.log("Failed to add favorite movie");
+                }
+            })
+            .then((data) => {
+                if (data) {
+                    alert("successfully added to favorites");
+                    localStorage.setItem("user", JSON.stringify(data));
+                    setFavorites(data.FavoriteMovies)
+                    setUser(data);
+                    setIsFavorite(true);
+                    console.log("successfully added to favs");
+                }
+            })
+            .catch((err) => {
+                alert(err);
+                console.error(err);
+            });
+    };
+
+    const removeFavoriteMovie = () => {
+        fetch(
+            `https://movie-flix-f31fbb6efa26.herokuapp.com/users/${user.Username}/movies/${movie.id}`,
+            { method: "DELETE", headers: { Authorization: `Bearer ${token}` } }
+        )
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    alert("Failed");
+                }
+            })
+            .then((user) => {
+                if (user) {
+                    alert("successfully deleted from favorites");
+                    localStorage.setItem("user", JSON.stringify(user));
+                    setUser(user);
+                    setIsFavorite(false);
+                }
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    };
 
     return (
         <Card>
@@ -37,6 +98,14 @@ export const MovieView = ({ movies, user, token, setUser }) => {
                     <Button className="back-button" variant='primary' style={{ cursor: "pointer" }} >Back</Button>
                 </Link>
             </Card.Body>
+            <Card.Body>
+                {!isFavorite ? (
+                    <Button className="primary" style={{ cursor: "pointer" }} onClick={addFavoriteMovie}>+Add to Favorites</Button>
+                ) : (
+                    <Button className="secondary" style={{ cursor: "pointer" }} onClick={removeFavoriteMovie}>Remove from Favorites</Button>
+                )}
+            </Card.Body>
+
         </Card>
 
 
